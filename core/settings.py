3519941c49a52a0c352b87dc324d347b4168af40
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2021-09-29 10:22:35 +0300

    Static + media setting

--- core/settings.py
+++ core/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'address_book/static')
 ]
@@ -122,3 +124,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
